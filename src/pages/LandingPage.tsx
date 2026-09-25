2bef5be8f8d3fc13d2f9763d59224ab4e564be4e
Samuel <109113326+samuxka@users.noreply.github.com>
2026-09-25 12:22:51 +0100

    url

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -141,7 +141,9 @@ export function LandingPage({
                 Siga-nos
               </p>
               <div className="flex items-center gap-3">
-                <img src={imgInstagram} alt="Instagram" className="w-5 h-5" />
+                <a href="https://www.instagram.com/bris_pastelaria/">
+                  <img src={imgInstagram} alt="Instagram" className="w-5 h-5" />
+                </a>
                 <img src={imgFacebook} alt="Facebook" className="w-5 h-5" />
                 <img src={imgTiktok} alt="TikTok" className="w-5 h-5" />
               </div>
